3b14c5e50df4cdaa358a53f6eb89ce108b299b48
tana-gh <yozoranotakaku@gmail.com>
2021-10-05 17:48:20 +0900

    Update test

--- app/src/test/scala/Test.scala
+++ app/src/test/scala/Test.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.{
   Await,
   ExecutionContext,
@@ -18,9 +19,15 @@ import fpsample_scala.lib.{
   runApp
 }
 
+
+enum TestEvent:
+  case ReadCalled
+  case WriteCalled
+
+import TestEvent.*
+
 case class TestState(
-  var readCalled : Boolean = false,
-  var writeCalled: Boolean = false
+  events: ListBuffer[TestEvent] = ListBuffer[TestEvent]()
 )
 
 case class TestApp(state: TestState)
@@ -28,19 +35,18 @@ case class TestApp(state: TestState)
 given DataReader[TestApp] with
   extension (app: TestApp)
     def read(using ExecutionContext): Future[Data] =
-      app.state.readCalled = true
+      app.state.events += ReadCalled
       Future.successful(Data("foo", 1))
 
 given DataWriter[TestApp] with
   extension (app: TestApp)
     def write(data: Data)(using ExecutionContext): Future[Unit] =
-      app.state.writeCalled = true
+      app.state.events += WriteCalled
       Future.successful(())
 
 class SetSuite extends AnyFunSuite:
   test("readData and writeData are called correctly") {
     val state = TestState()
     Await.ready(runApp(TestApp(state)), Duration.Inf)
-    assert(state.readCalled)
-    assert(state.writeCalled)
+    assertResult(List(ReadCalled, WriteCalled))(state.events.toList)
   }
